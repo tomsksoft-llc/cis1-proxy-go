eba2f678e784d378388c5922534ba0d5929c85d9
ghostinecatnewyear <52239723+ghostinecatnewyear@users.noreply.github.com>
2020-04-07 15:00:58 +0700

    Change code style and routing config format

--- internal/requestfiltration/filter.go
+++ internal/requestfiltration/filter.go
@@ -3,67 +3,44 @@ package requestfiltration
 import (
 	"net"
 	"net/http"
-	"regexp"
 	"strings"
 )
 
-var (
-	passPathSearch, _    = regexp.Compile(`http://[.\S]+:\d+(/[.\S]*)`)
-	passHostSearch, _    = regexp.Compile(`http://([.\S]+):\d+/[.\S]*`)
-	passAddressSearch, _ = regexp.Compile(`http://([.\S]+:\d+)/[.\S]*`)
-	ipSearch, _          = regexp.Compile(`\d+.\d+.\d+.\d+`)
-)
-
 type Filter interface {
-	FilterRequest(request *http.Request) bool
-	GetLastPassAddress() string
-	AddRoute(route Route)
+	Filter(req *http.Request) (passAddress string, isPassed bool)
+	AddRoutes(routes []Route)
 }
 
 func NewFilter() Filter {
-	return &filter{routes: make(map[string]string)}
+	return &filter{routes: make(map[string]RoutePass)}
 }
 
 type filter struct {
-	routes          map[string]string
-	lastPassAddress string
+	routes map[string]RoutePass
 }
 
-func (this *filter) FilterRequest(request *http.Request) bool {
-	for location, pass := range this.routes {
+func (f *filter) Filter(req *http.Request) (passAddress string, isPassed bool) {
+	for location, _ := range f.routes {
 
-		if 0 == strings.Index(request.URL.Path, location) {
-			this.modifyRequest(request, location)
-			this.lastPassAddress = passAddressSearch.FindStringSubmatch(pass)[1]
+		if 0 == strings.Index(req.URL.Path, location) {
+			f.modifyRequest(req, location)
 
-			return true
+			passAddress = net.JoinHostPort(f.routes[location].Host, f.routes[location].Port)
+			return passAddress, true
 		}
 	}
 
-	this.lastPassAddress = ""
-	return false
+	return "", false
 }
 
-func (this *filter) GetLastPassAddress() string {
-	return this.lastPassAddress
-}
-
-func (this *filter) AddRoute(route Route) {
-	// Route.Pass = resolvePassHost(Route.Pass)
-	this.routes[route.Location] = route.Pass
-}
-
-func (this *filter) modifyRequest(request *http.Request, location string) {
-	request.URL.Path = strings.Replace(request.URL.Path, location, passPathSearch.FindStringSubmatch(this.routes[location])[1], 1)
-	request.Host = passHostSearch.FindStringSubmatch(this.routes[location])[1]
-	request.Header.Add("Connection", "close")
-}
-
-func resolvePassHost(pass string) {
-	var passHost = passHostSearch.FindStringSubmatch(pass)[1]
-
-	if ipSearch.MatchString(passHost) {
-		var resolveResult, _ = net.LookupAddr(passHost)
-		pass = strings.Replace(pass, passHost, resolveResult[0], 1)
+func (f *filter) AddRoutes(routes []Route) {
+	for _, route := range routes {
+		f.routes[route.Location] = route.Pass
 	}
+}
+
+func (f *filter) modifyRequest(req *http.Request, location string) {
+	req.URL.Path = strings.Replace(req.URL.Path, location, f.routes[location].Path, 1)
+	req.Host = f.routes[location].Host
+	req.Header.Add("Connection", "close")
 }
